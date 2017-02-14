158f1d697f7b8656beb58b204e0877eae5d64677
Evin Grost <evin.grost@gmail.com>
2017-02-14 16:08:48 -0600

    Testing timer.

--- scripts.js
+++ scripts.js
@@ -1,20 +1,58 @@
 window.addEventListener('load', function(){
+	var startTimer = Date.now();
 
 	function getXY(e){
 		if((e.clientX > 668) && (e.clientX < 712) && (e.clientY > 139) && (e.clientY < 193)){
-			console.log("Yep you got it! You're at " + e.clientX + ", " + e.clientY);
+			endTimer();
 		}
 
 		else {
-			console.log("Nope, you're at " + e.clientX + ", " + e.clientY);
 		}
 	}
 
-	function startTimer(){
-		alert("I'm loaded!");
+	function endTimer(){
+		var endingTime = Date.now();
+		var totalTime = endingTime - startTimer;
+		convertToReadableTime(totalTime);
+	}
+
+	function convertToReadableTime(millis){
+		var tenths = millis / 1000;
+		var x = millis / 1000;
+		var seconds = x % 60;
+		x /= 60;
+		var minutes = x % 60;
+		x /= 60;
+		var hours = x % 24;
+		x /= 24;
+		var days = x;
+		alert(minutes + "." + seconds + "." + tenths);
 	}
 
 	var picClick = document.getElementById("WaldoLevel1");
 	picClick.addEventListener('click', getXY);
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
